5a7a128a2974210f616f5115fcb1d79bd2d55b9c
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2025-11-19 22:06:53 +0100

    (Berat) added SchoolClass and Room

---
+++ src/Stundenplaner-Projekt/Stundenplaner-Projekt/Room.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace Stundenplaner_Projekt
+{
+    internal class Room
+    {
+        private string _roomId;
+        private int _maxStudent;
+        public string RoomId
+        {
+            get
+            {
+                return _roomId;
+            }
+            set
+            {
+                _roomId = value;
+            }
+        }
+        public int MaxStudent
+        {
+            get
+            {
+                return _maxStudent;
+            }
+            set
+            {
+                _maxStudent = value;
+            }
+        }
+        public Room(string roomId)
+        {
+            _roomId = roomId;
+        }
+        public Room(string roomId, int maxStudent)
+        {
+            _roomId = roomId;
+            _maxStudent = maxStudent;
+        }
+    }
+}
---
+++ src/Stundenplaner-Projekt/Stundenplaner-Projekt/SchoolClass.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Stundenplaner_Projekt
+{
+    internal class SchoolClass
+    {
+        private string _name;
+        private List<Student> _students;
+        private Dictionary<TimeBlock, Combination> _timetable;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public List<Student> Students
+        {
+            get
+            {
+                return _students;
+            }
+            set
+            {
+                _students = value;
+            }
+        }
+        public Dictionary<TimeBlock, Combination> Timetable
+        {
+            get
+            {
+                return _timetable;
+            }
+            set
+            {
+                _timetable = value;
+            }
+        }
+        public SchoolClass(string name)
+        {
+            _name = name;
+        }
+        public SchoolClass(string name, List<Students> students)
+        {
+            _name = name;
+            _students = students;
+        }
+        public void AddStudent(Student student) => Students.Add(student);
+        public void RemoveSTudent(Student student) => Students.Remove(student);
+    }
+}
